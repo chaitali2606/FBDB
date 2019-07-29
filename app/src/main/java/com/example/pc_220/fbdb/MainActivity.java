60e53367ee13737d9b846ef3af9ca25e7d1e4eef
chaitali <chaitali.choudhary@gmail.com>
2019-07-29 07:36:02 +0500

    second commit

--- app/src/main/java/com/example/pc_220/fbdb/MainActivity.java
+++ app/src/main/java/com/example/pc_220/fbdb/MainActivity.java
@@ -1,13 +1,25 @@
 package com.example.pc_220.fbdb;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button clickButton = (Button) findViewById(R.id.button);
+        clickButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Intent i=new Intent(MainActivity.this,New.class);
+                startActivity(i);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/pc_220/fbdb/New.java
@@ -0,0 +1,12 @@
+package com.example.pc_220.fbdb;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class New extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new);
+    }
+}
